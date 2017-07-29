e6a255a7182f38a1b35aab9912d92c9d9cde0241
Sven <sven@139.com>
2017-07-29 15:52:30 +0800

    Update packages

--- index.js
+++ index.js
@@ -1,76 +1,72 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Link, IndexLink, hashHistory } from 'react-router'
+import { Router, Route, Link, IndexLink, browserHistory } from 'react-router'
 import { Redirect } from 'react-router'
 import { IndexRoute } from 'react-router'
-import { IndexRedirect,withRouter } from 'react-router'
+import { IndexRedirect, withRouter } from 'react-router'
+import PropTypes from 'prop-types'
 
-const App = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-        <ul>
-          <li><IndexLink to="/">Dashboard</IndexLink></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/inbox">Inbox</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
-})
+const App = (props) => (
+  <div>
+    <h1>App</h1>
+    <ul>
+      <li><IndexLink to="/">Dashboard</IndexLink></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/inbox">Inbox</Link></li>
+    </ul>
+    {props.children}
+  </div>
+)
+App.proptypes = {
+  children : PropTypes.node
+}
+const About = () => (
+  <h3>About</h3>
+)
 
-const About = React.createClass({
-  render() {
-    return <h3>About</h3>
-  }
-})
+const Inbox = (props) => (
+  <div>
+    <h2>Inbox</h2>
+    <Link to="/inbox/messages/5">messages</Link>
+    {props.children}
+  </div>
+)
 
-const Inbox = React.createClass({
-  render() {
-    return (
-      <div>
-        <h2>Inbox</h2>
-        <Link to="/inbox/messages/5">messages</Link>
-        {this.props.children}
-      </div>
-    )
-  }
-})
-
-const Dashboard = React.createClass({
-  render() {
-    return <div>Welcome to the app!</div>
-  }
-})
+Inbox.proptypes = {
+  children : PropTypes.node
+}
+const Dashboard = () => (
+  <div>Welcome to the app!</div>
+)
 
 const InboxHome = () => (
-    <div>
-      <p>Welcome to Inbox!</p>
-    </div>
-  )
+  <div>
+    <p>Welcome to Inbox!</p>
+  </div>
+)
 
-const Message_1 = React.createClass({
-  render() {
-    return <h3>Message {this.props.params.id}</h3>
-  }
-})
-const Message = withRouter(Message_1)
+const Message_1 = ({params}) => (
+  <h3>Message {params.id}</h3>
+)
+
+Message_1.propTypes = {
+  params : PropTypes.object.isRequired
+}
+const Message   = withRouter(Message_1)
 
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Dashboard} />
-      <Route path="about" component={About} />
+      <IndexRoute component={Dashboard}/>
+      <Route path="about" component={About}/>
       <Route path="inbox" component={Inbox}>
-        <IndexRoute component={InboxHome} />
-        <Redirect from="messages/:id" to="/messages/:id" />
+        <IndexRoute component={InboxHome}/>
+        <Redirect from="messages/:id" to="/messages/:id"/>
         {/*<IndexRedirect to='/about' />*/}
 
       </Route>
       <Route component={Inbox}>
-        <Route path="messages/:id" component={Message} />
+        <Route path="messages/:id" component={Message}/>
       </Route>
     </Route>
   </Router>
