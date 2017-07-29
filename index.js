0b4daceb7d5f566661425d617ed202b6e254854c
Sven <sven@139.com>
2017-07-29 16:40:03 +0800

    Update packages

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { IndexRoute } from 'react-router'
 import { IndexRedirect, withRouter } from 'react-router'
 import PropTypes from 'prop-types'
 
-const App = (props) => (
+const App     = (props) => (
   <div>
     <h1>App</h1>
     <ul>
@@ -20,7 +20,7 @@ const App = (props) => (
 App.proptypes = {
   children : PropTypes.node
 }
-const About = () => (
+const About   = () => (
   <h3>About</h3>
 )
 
@@ -45,14 +45,23 @@ const InboxHome = () => (
   </div>
 )
 
-const Message_1 = ({params}) => (
+const Message_1 = ({ params }) => (
   <h3>Message {params.id}</h3>
 )
 
 Message_1.propTypes = {
   params : PropTypes.object.isRequired
 }
-const Message   = withRouter(Message_1)
+const Message       = withRouter(Message_1)
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+/* 路由配置 */
+const RouteConfig =
+        (<BrowserRouter>
+          <Route path="/" component={Main}/>
+          <Route path="/dataspace" component={Data}/>
+        </BrowserRouter>)
 
 render((
   <Router history={browserHistory}>
